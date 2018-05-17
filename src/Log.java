37008e0c9b6b44679b4ceb18adcc0053cdc5b629
LiZhen <thereaghostflash@gmail.com>
2018-05-17 13:21:09 -0400

    Add files via upload

---
+++ src/Log.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Log {
+
+    public static void log(String time,String location,String people,String operation){
+        File file = new File("./log");
+        if (!file.exists()){
+            file.mkdir();
+        }
+        File inforFile = new File("./log/" + people + ".txt");
+
+        BufferedWriter bufferedWriter = null;
+        String information = time + ":" + operation + " on" + location;
+        if (inforFile.exists()){
+            try {
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./log/" + people + ".txt",  true )));
+                bufferedWriter.write(information);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            try {
+                inforFile.createNewFile();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./log/" + people + ".txt",  true )));
+                bufferedWriter.write(information);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
