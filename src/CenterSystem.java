16ce8d901cde4d4a383096c7234fafda10a943b1
thereaghostflash <jeanlee@JeandeMacBook-Pro.local>
2018-05-20 13:46:01 -0400

    add log into edit record

--- src/CenterSystem.java
+++ src/CenterSystem.java
@@ -10,7 +10,7 @@ import java.beans.Statement;
 import java.beans.PropertyDescriptor;
 
 public class CenterSystem extends UnicastRemoteObject implements CenterServer {
-
+    private String centerName = "";
     protected  ConcurrentHashMap<Character,ArrayList<Records>> database = new ConcurrentHashMap<>();
     private static Registry centerRegistry;
 
@@ -24,6 +24,14 @@ public class CenterSystem extends UnicastRemoteObject implements CenterServer {
 
     public CenterSystem() throws RemoteException {
         super();
+    }
+
+    public String getCenterName() {
+        return centerName;
+    }
+
+    public void setCenterName(String centerName) {
+        this.centerName = centerName;
     }
 
     public void createTRecord(String managerId, String firstName, String lastName, String address, int phone, String specialization, String location) throws RemoteException {
@@ -71,7 +79,7 @@ public class CenterSystem extends UnicastRemoteObject implements CenterServer {
 
     public String editRecord(String managerId,String recordID, String fieldName, String newValue) throws Exception {
         String result = "";
-        Boolean ableModified = false;
+        Boolean ableModified = true;
 
         for (char key : database.keySet()){
             for (Records record : database.get(key)){
@@ -106,13 +114,21 @@ public class CenterSystem extends UnicastRemoteObject implements CenterServer {
                                 Statement stmt = new Statement(record, prop.getWriteMethod().getName(), new Object[]{newValue});
                                 stmt.execute();
                                 result = "Record updated";
+                                String operation = "edit: " + prop.getName();
+                                Log.log(Log.getCurrentTime(),centerName,managerId,operation,"Successful");
                                 return result;
+                            }else {
+                                String operation = "edit: " + prop.getName();
+                                Log.log(Log.getCurrentTime(),centerName,managerId,operation,"Failed");
+                                return result = "The newValue is not valided!";
                             }
                         }
-                        else{
-                            result = "fieldName doesn't match record type";
-                        }
+
                     }
+                    result = "fieldName doesn't match record type";
+                    String operation = "edit: " + fieldName;
+                    Log.log(Log.getCurrentTime(),centerName,managerId,operation,"Failed");
+
 
                 }
             }
--- src/Log.java
+++ src/Log.java
@@ -1,6 +1,16 @@
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Log {
+
+    public static String getCurrentTime(){
+        Date date=new Date();
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate=dateFormat.format(date);
+        return formattedDate;
+    }
 
     public static void log(String time,String location,String people,String operation,String result){
         File file = new File("./log");
